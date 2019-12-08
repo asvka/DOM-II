ff96772503d8ce103aa939852e2f2475f2295b91
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2019-12-08 15:23:31 -0800

    mouseover complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const h1Fade = document.querySelector('h1');
+h1Fade.addEventListener('mouseover', (event)=>{
+    event.target.style.color = 'darkgrey';
+  })
+h1Fade.addEventListener('mouseout', (event)=>{
+    event.target.style.color = 'black';
+})

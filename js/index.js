52486e2cba8b5ec0ec05d6c35b81e73c3ab2628f
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2019-12-08 17:09:43 -0800

    Added mouseout, keydown, and touchstart

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@ h1Fade.addEventListener('mouseover', (event)=>{
 h1Fade.addEventListener('mouseout', (event)=>{
     event.target.style.color = 'black';
 })
+addEventListener('keydown', (event) => {
+    if (event.keyCode == 55)
+      alert('My favorite number!')
+  })
+//   addEventListener("keyup", function(event) {
+//     if (event.keyCode == 68)
+//       document.body.style.background = "";
+//   });
+// addEventListener('wheel', (event) =>{
+//     event.preventDefault();
+// })
+
+// const headerBar = document.getElementsByTagName('header');
+const darkMode = addEventListener('touchstart', (event) =>{
+    document.body.style.backgroundColor = 'black'
+    document.body.style.color = 'white'
+})

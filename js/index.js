ea923504a797c492150f08ad8c41ead8110f0cb7
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2019-12-08 18:22:45 -0800

    Progress...

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // Your code goes here
 const h1Fade = document.querySelector('h1');
 h1Fade.addEventListener('mouseover', (event)=>{
-    event.target.style.color = 'darkgrey';
+    event.target.style.color = 'black';
   })
 h1Fade.addEventListener('mouseout', (event)=>{
-    event.target.style.color = 'black';
+    event.target.style.color = 'darkgrey';
 })
 addEventListener('keydown', (event) => {
     if (event.keyCode == 55)
@@ -21,5 +21,12 @@ addEventListener('keydown', (event) => {
 // const headerBar = document.getElementsByTagName('header');
 const darkMode = addEventListener('touchstart', (event) =>{
     document.body.style.backgroundColor = 'black'
-    document.body.style.color = 'white'
+    document.body.style.color = 'darkgrey'
+    h1Fade.style.color = 'black'
+})
+
+const loadScreen = document.querySelectorAll('h1', 'h2', 'h3', 'h4');
+loadScreen.addEventListener('load', (event) => {
+    event.target.style.opacity = '50'
+    // event.target.style.opacity = '100'
 })
